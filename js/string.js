748d3f13a4b98248bf25d8953ee2dde6d4b5a892
Lakei87 <monastyrskyis@gmail.com>
2022-02-10 22:34:33 +0100

    Update string.js

--- js/string.js
+++ js/string.js
@@ -1,24 +1,59 @@
-// task1
+// task 1
 // const str = "Привет, Мир!";
 // console.log(str);
 
-// task2
+// task 2
 // const str1 = "Привет, ";
 // const str2 = "Мир!";
 // console.log(str1 + str2);
 
-// task3
+// task 3
 // const name = "Serhii";
 // console.log(`Привет, ${name}!`);
 
-// // task4
+// task 4
 // const age = 35;
 // console.log(`Мне ${age} лет!`);
 
-// task5
+// task 5
 // let askName = prompt("Your name");
 // alert(askName);
 
-// task6
-const askNumber = prompt("Imput number");
-alert(Math.pow(askNumber, 3));
+// task 6
+// const askNumber = prompt("Imput number");
+// alert(Math.pow(askNumber, 3));
+
+// task 7
+// const str = "abcde";
+// console.log(str[0]);
+// console.log(str[2]);
+// console.log(str[4]);
+
+// task 8
+// const num = "12345";
+// console.log(num[0] * num[1] * num[2] * num[3] * num[4]);
+
+// task 9
+// const seconds = 60;
+// const minutes = 60;
+// const hours = 24;
+// const mounth = 30;
+// const secondsPerHour = seconds * minutes;
+// const secondsPerDay = secondsPerHour * hours;
+// const secondsPerMounth = secondsPerDay * mounth;
+// console.log(`количество секунд в часе ${secondsPerHour}`);
+// console.log(`количество секунд в сутке ${secondsPerDay}`);
+// console.log(`количество секунд в месяце ${secondsPerMounth}`);
+
+// task 10
+// const seconds = 45;
+// const minutes = 53;
+// const hours = 21;
+// console.log(`текущее время ${hours}:${minutes}:${seconds}`);
+
+// task 11
+// const num = 7;
+// console.log(Math.pow(num, 2));
+
+let a = (3 * 2) ** 3;
+alert(a);
