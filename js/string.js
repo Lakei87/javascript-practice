e03488f5e0cfa34b348933eeb39def0db741aa9d
Lakei87 <monastyrskyis@gmail.com>
2022-02-10 21:00:58 +0100

    создает файлы и папки для работы

---
+++ js/string.js
@@ -0,0 +1,24 @@
+// task1
+// const str = "Привет, Мир!";
+// console.log(str);
+
+// task2
+// const str1 = "Привет, ";
+// const str2 = "Мир!";
+// console.log(str1 + str2);
+
+// task3
+// const name = "Serhii";
+// console.log(`Привет, ${name}!`);
+
+// // task4
+// const age = 35;
+// console.log(`Мне ${age} лет!`);
+
+// task5
+// let askName = prompt("Your name");
+// alert(askName);
+
+// task6
+const askNumber = prompt("Imput number");
+alert(Math.pow(askNumber, 3));
---
+++ js/var.js
@@ -0,0 +1,32 @@
+// task1
+// const num = 3;
+// alert(num);
+
+// task2
+// const a = 10;
+// const b = 2;
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a % b);
+
+// task3
+// const c = 15;
+// const d = 2;
+// const result = c + d;
+// console.log(result);
+
+// task 4
+// const a = 10;
+// const b = 2;
+// const c = 5;
+// console.log(a + b + c);
+
+// task5
+// const a = 17;
+// const b = 10;
+// const c = a - b;
+// const d = 7;
+// const result = c * d;
+// console.log(result);
